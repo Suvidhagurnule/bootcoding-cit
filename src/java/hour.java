2acf9fb2b8185a7bb163df59cf608eb72d67d6ed
dell <gurnulesuvidha@gmail.com>
2023-08-17 15:57:41 +0530

    java program

---
+++ src/java/hour.java
@@ -0,0 +1,10 @@
+public class hour {
+    public static void main(String[] args) {
+        int hours = 2;
+        int minutes = 5;
+        int seconds = minutes * 60;
+        int hourseconds = hours * 60 * 60;
+        System.out.println(" hours to seconds :" + hourseconds);
+        System.out.println("minutes to seconds :" + seconds);
+    }
+}
